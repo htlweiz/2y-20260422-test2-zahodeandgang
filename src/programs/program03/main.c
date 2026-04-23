a75b54797e3963748ec2c31792438e547165e224
Zahode <riya.hayashi@sr.htlweiz.at>
2026-04-23 08:56:08 +0000

    programm 3

--- src/programs/program03/main.c
+++ src/programs/program03/main.c
@@ -4,15 +4,22 @@
 
 #include <inttypes.h>
 #include <stdio.h>
+#include <math.h>
 
 // TODO: your code goes here
+double Celsius2Fahrenheit (double temp_celsius) {
+  double temp_fahrenheit;
+  temp_fahrenheit = (temp_celsius * (1.8)) + 32;
+  return temp_fahrenheit;
+}
 
 int main(void) {
   double temp_celsius = 0;
 
+  printf("Temperatur in Grad Celsius: ");
   scanf("%lf", &temp_celsius);
 
-  printf("%f", Celsius2Fahrenheit(temp_celsius));
+  printf("Temperatur in Grad Fahrenheit: %.1f\n", Celsius2Fahrenheit(temp_celsius));
 
   return 0;
 }
